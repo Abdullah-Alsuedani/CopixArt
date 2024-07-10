e58ea77cd8198491fc701a3dd5e02b0474784166
Abdullah Alsuedani <you@example.com>
2024-07-10 18:46:57 +0300

    'gds'

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-setTimeout(()=>{
 
-    let logo = document.querySelector('spline-viewer div')
-    console.log(logo)
-}, 2000)
+
+let cursor = document.querySelector('.cursor')
+let titleEls = document.querySelectorAll('.hero h1 span')
+
+document.addEventListener("mousemove", (e)=>{
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
+})
+
+titleEls.forEach((el)=>{
+        el.addEventListener('mouseover', ()=>{
+            el.style.color = 'var(--main-color)';
+            cursor.style.transform = 'scale(2)'
+            cursor.style.zIndex = '-1000'
+        })
+        el.addEventListener('mouseleave', ()=>{
+            el.style.color = 'white';
+            cursor.style.transform = 'scale(1)'
+            cursor.style.zIndex = '1000'
+        })
+})
