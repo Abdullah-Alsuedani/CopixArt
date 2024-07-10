bf9416306150abeee34dc21be9e9165f478816d6
Abdullah Alsuedani <you@example.com>
2024-07-10 19:20:02 +0300

    'df'

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
 let cursor = document.querySelector('.cursor')
 let titleEls = document.querySelectorAll('.hero h1 span')
 
+let mainTitle = document.querySelector('.hero h1')
+let mainP = document.querySelector('.hero p')
+let mainbtns = document.querySelector('.hero .but')
+let mainImage = document.querySelector('.hero-img img')
+
 document.addEventListener("mousemove", (e)=>{
     cursor.style.left = `${e.clientX}px`;
     cursor.style.top = `${e.clientY}px`;
@@ -19,4 +24,16 @@ titleEls.forEach((el)=>{
             cursor.style.transform = 'scale(1)'
             cursor.style.zIndex = '1000'
         })
+})
+
+function mainAnim(el, d){
+    el.style.transform = `translate${d}(0)`
+    el.style.opacity = 1
+}
+
+window.addEventListener('load', () => {
+    mainAnim(mainTitle, 'X')
+    mainAnim(mainP, 'X')
+    mainAnim(mainbtns, 'Y')
+    mainAnim(mainImage, 'Y')
 })
